56b70507635955b360bb80bdb590ee7c715d914b
Koen Samyn <samyn.koen@gmail.com>
2018-01-07 12:48:47 +0100

    added Override tags. marked fields final. diamond inference. changes related to FXProject system.

--- src/main/java/dae/fxcreator/node/NodeContainer.java
+++ src/main/java/dae/fxcreator/node/NodeContainer.java
@@ -12,44 +12,47 @@ import java.util.List;
 /**
  * This class represents a grouping of nodes. This can be used to implement an
  * iterator block, or function.
+ *
  * @author Koen
  */
 public class NodeContainer extends ShaderNode implements NodeGroup, TypedNode {
-    private ArrayList<IONode> nodes = new ArrayList<IONode>();
-    private ArrayList<ReferenceNode> referenceNodes =new ArrayList<ReferenceNode>();
-    private HashMap<String, IONode> nodeMap = new HashMap<String, IONode>();
-    private ShaderNode inputNode;
-    private ShaderNode outputNode;
+
+    private final ArrayList<IONode> nodes = new ArrayList<>();
+    private final ArrayList<ReferenceNode> referenceNodes = new ArrayList<>();
+    private final HashMap<String, IONode> nodeMap = new HashMap<>();
+    private final ShaderNode inputNode;
+    private final ShaderNode outputNode;
 
     private String subType;
 
     /**
-     * Gets the subtype of this node container. The subtype is used to get the correct
-     * method definition file.
+     * Gets the subtype of this node container. The subtype is used to get the
+     * correct method definition file.
+     *
      * @return the subtype.
      */
+    @Override
     public String getSubType() {
         return subType;
     }
 
-
-
     /**
      * Sets the subtype for this node container.
+     *
      * @param subType the new subtype.
      */
+    @Override
     public void setSubType(String subType) {
         this.subType = subType;
     }
-    
+
     /**
      * Creates a new NodeContainer object.
      */
-    public NodeContainer(String name, String type,ShaderTypeLibrary library)
-    {
-        super(name,name,type,library);
-        inputNode = new ShaderNode("input", "input", "group.input",library);
-        outputNode = new ShaderNode("output", "output", "group.output",library);
+    public NodeContainer(String name, String type) {
+        super(name, name, type, null);
+        inputNode = new ShaderNode("input", "input", "group.input", null);
+        outputNode = new ShaderNode("output", "output", "group.output", null);
         inputNode.setSeparator(".");
         outputNode.setSeparator(".");
 
@@ -62,31 +65,34 @@ public class NodeContainer extends ShaderNode implements NodeGroup, TypedNode {
 
     /**
      * Returns the input struct name for this node container.
+     *
      * @return the name for the input struct of the method.
      */
-    public String getInputStructName(){
+    public String getInputStructName() {
         return getMethodName() + "_INPUT";
     }
 
     /**
      * Returns the output struct name for this node container.
+     *
      * @return the name for the output struct of the method.
      */
-    public String getOutputStructName(){
+    public String getOutputStructName() {
         return getMethodName() + "_OUTPUT";
     }
 
     /**
      * Returns the name of the method name.
+     *
      * @return the name of the method.
      */
-    public String getMethodName(){
+    public String getMethodName() {
         return this.getSubType().replace(".", "_");
     }
 
     /**
-     * Creates the node connections between the nodes.
-     * This only works if all the nodes are loaded into the shader stage.
+     * Creates the node connections between the nodes. This only works if all
+     * the nodes are loaded into the shader stage.
      */
     public void connectNodes() {
         for (IONode node : nodes) {
@@ -97,6 +103,7 @@ public class NodeContainer extends ShaderNode implements NodeGroup, TypedNode {
 
     /**
      * Create the connections for a single node.
+     *
      * @param node the node to connect.
      */
     private void connectNode(IONode node) {
@@ -112,41 +119,45 @@ public class NodeContainer extends ShaderNode implements NodeGroup, TypedNode {
                     output = this.inputNode.findOutput(outputId);
                 } else {
                     IONode on = this.findNode(nodeId);
-                    if (on != null)
+                    if (on != null) {
                         output = on.findOutput(outputId);
+                    }
                 }
-                if ( output != null)
+                if (output != null) {
                     input.setConnectedInput(output);
+                }
             }
         }
     }
 
     @Override
     public Object clone() throws CloneNotSupportedException {
-        NodeContainer node = new NodeContainer("group", this.getType(),shaderTypeLib);
+        NodeContainer node = new NodeContainer("group", this.getType());
         node.setInputOutputEditable(this.isInputOutputEditable());
         // copy inputs
         List<ShaderInput> inputs = this.getInputs();
         for (ShaderInput input : inputs) {
             String semantic = null;
-            if ( input.getSemantic().isValid())
-                semantic= input.getSemantic().toString();
-            ShaderInput copy = new ShaderInput(node, input.getName(), semantic,input.getType(),input.getAcceptTypeSet());
+            if (input.getSemantic().isValid()) {
+                semantic = input.getSemantic().toString();
+            }
+            ShaderInput copy = new ShaderInput(node, input.getName(), semantic, input.getType(), input.getAcceptTypeSet());
             copy.setConnectionString(input.getConnectionString());
             node.addInput(copy);
         }
         List<ShaderOutput> outputs = this.getOutputs();
         for (ShaderOutput output : outputs) {
             String semantic = null;
-            if ( output.getSemantic().isValid())
-                semantic= output.getSemantic().toString();
-            ShaderOutput copy = new ShaderOutput(node, output.getName(),semantic, output.getType(),output.getTypeRule());
+            if (output.getSemantic().isValid()) {
+                semantic = output.getSemantic().toString();
+            }
+            ShaderOutput copy = new ShaderOutput(node, output.getName(), semantic, output.getType(), output.getTypeRule());
             copy.setConnectionString(output.getConnectionString());
             node.addOutput(copy);
         }
-        for (IONode child : this.getNodes()){
-            IONode clonedNode = (IONode)child.clone();
-            node.addNode( clonedNode );
+        for (IONode child : this.getNodes()) {
+            IONode clonedNode = (IONode) child.clone();
+            node.addNode(clonedNode);
             clonedNode.setPosition(child.getPosition().x, child.getPosition().y);
         }
 
@@ -161,15 +172,16 @@ public class NodeContainer extends ShaderNode implements NodeGroup, TypedNode {
         return node;
     }
 
-     /**
+    /**
      * Adds an input to this NodeContainer object and also adds it to the
      * inputNode object that is a member of this ShaderStage.
+     *
      * @param input the new input to add.
      */
     @Override
     public boolean addInput(ShaderInput input) {
         if (super.addInput(input)) {
-            ShaderOutput o = new ShaderOutput(inputNode, input.getName(), input.getSemantic().getValue(), input.getType(),null);
+            ShaderOutput o = new ShaderOutput(inputNode, input.getName(), input.getSemantic().getValue(), input.getType(), null);
             o.setConnectionString(input.getConnectionString());
             inputNode.addOutput(o);
             return true;
@@ -180,6 +192,7 @@ public class NodeContainer extends ShaderNode implements NodeGroup, TypedNode {
 
     /**
      * Removes an input from this ShaderStage object.
+     *
      * @param input the input to remove.
      */
     @Override
@@ -188,9 +201,10 @@ public class NodeContainer extends ShaderNode implements NodeGroup, TypedNode {
         inputNode.removeOutput(input.getName());
     }
 
-     /**
+    /**
      * Adds an output to this ShaderStage object and also adds it to the
      * outputNode object that is a member of this ShaderStage.
+     *
      * @param output the new input to add.
      */
     @Override
@@ -206,8 +220,9 @@ public class NodeContainer extends ShaderNode implements NodeGroup, TypedNode {
     }
 
     /**
-     * Removes an output from the output list and removes it as an input
-     * from the output node.
+     * Removes an output from the output list and removes it as an input from
+     * the output node.
+     *
      * @param output the ShaderOutput object to remove.
      */
     @Override
@@ -216,19 +231,22 @@ public class NodeContainer extends ShaderNode implements NodeGroup, TypedNode {
         outputNode.removeInput(output.getName());
     }
 
-
     /**
      * Adds a node to this NodeContainer object.
+     *
      * @param node the node to add.
      */
     public void addNode(IONode node) {
         nodes.add(node);
         nodeMap.put(node.getId(), node);
-        node.setFXProject(getFXProject());
+        if (getFXProject() != null) {
+            node.setFXProject(getFXProject());
+        }
     }
 
     /**
      * Removes a node from this NodeContainer object.
+     *
      * @param node the node to remove.
      */
     public void removeNode(IONode node) {
@@ -236,14 +254,15 @@ public class NodeContainer extends ShaderNode implements NodeGroup, TypedNode {
         nodeMap.remove(node.getId());
     }
 
-     /**
+    /**
      * Finds a node in the list of nodes.
+     *
      * @param id the id for the node.
      * @return the ShaderNode with the provided id, or null if no ShaderNode was
      * found.
      */
     public IONode findNode(String id) {
-       return nodeMap.get(id);
+        return nodeMap.get(id);
     }
 
     public Iterable<IONode> getNodes() {
@@ -259,13 +278,14 @@ public class NodeContainer extends ShaderNode implements NodeGroup, TypedNode {
     }
 
     public Iterable<IONode> getSortedNodes() {
-       NodeGroupWalker walker = new NodeGroupWalker(this);
-       walker.collect();
-       return walker.getNodes();
+        NodeGroupWalker walker = new NodeGroupWalker(this);
+        walker.collect();
+        return walker.getNodes();
     }
 
-     /**
+    /**
      * Called when the name of a ShaderInput object has changed.
+     *
      * @param oldName the old name of the input.
      * @param input the ShaderInput that was changed.
      */
@@ -279,6 +299,7 @@ public class NodeContainer extends ShaderNode implements NodeGroup, TypedNode {
 
     /**
      * Called when the semantic of a ShaderInput object has changed.
+     *
      * @param oldSemantic the old semantic of the input.
      * @param input the ShaderInput that was changed.
      */
@@ -292,6 +313,7 @@ public class NodeContainer extends ShaderNode implements NodeGroup, TypedNode {
 
     /**
      * Called when the name of a ShaderOutput object has changed.
+     *
      * @param oldName the old name of the output.
      * @param output the ShaderOutput that was changed.
      */
@@ -305,6 +327,7 @@ public class NodeContainer extends ShaderNode implements NodeGroup, TypedNode {
 
     /**
      * Called when the semantic of a ShaderOutput object has changed.
+     *
      * @param oldSemantic the old semantic of the output.
      * @param output the ShaderOutput that was changed.
      */
@@ -318,6 +341,7 @@ public class NodeContainer extends ShaderNode implements NodeGroup, TypedNode {
 
     /**
      * Called when the type of an input or output changes.
+     *
      * @param io the input or output that was changed.
      */
     @Override
@@ -332,37 +356,39 @@ public class NodeContainer extends ShaderNode implements NodeGroup, TypedNode {
         }
     }
 
-    public String getContainerType()
-    {
+    public String getContainerType() {
         return "group";
     }
 
     public String getUniqueId(String prefix) {
         int i = 1;
         String id;
-        do{
+        do {
             id = prefix + i;
-        }while (this.nodeMap.containsKey(id));
+        } while (this.nodeMap.containsKey(id));
         return id;
     }
 
     /**
      * Add a node that is a reference to another node
+     *
      * @param rn the reference node to add.
      */
-    public void addReferenceNode(ReferenceNode rn){
+    public void addReferenceNode(ReferenceNode rn) {
         referenceNodes.add(rn);
     }
+
     /**
      * Removes the reference node from the list of reference nodes.
      */
-    public void removeReferenceNode(ReferenceNode rn){
+    public void removeReferenceNode(ReferenceNode rn) {
         referenceNodes.remove(rn);
     }
+
     /**
      * Returns the reference nodes.
      */
-    public Iterable<ReferenceNode> getReferenceNodes(){
+    public Iterable<ReferenceNode> getReferenceNodes() {
         return referenceNodes;
     }
 }
