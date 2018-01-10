8fbe0fe3ece6039a94d0051e0efaf533e59dfd6f
Koen Samyn <samyn.koen@gmail.com>
2018-01-10 10:09:31 +0100

    Added method to load the project from the template.

--- src/main/java/dae/fxcreator/io/FXProjectTemplate.java
+++ src/main/java/dae/fxcreator/io/FXProjectTemplate.java
@@ -1,8 +1,9 @@
 package dae.fxcreator.io;
 
+import dae.fxcreator.io.loaders.FXProjectLoader;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.File;
+import java.nio.file.Path;
 
 /**
  *
@@ -25,11 +26,11 @@ public class FXProjectTemplate implements ActionListener {
     /**
      * The name for the project template.
      */
-    private File sourceFile;
+    private Path sourceFile;
     /**
      * The relative path of the project template.
      */
-    private String relativePath;
+    private Path relativePath;
     /**
      * The group of the FXProjectTemplate object.
      */
@@ -43,6 +44,7 @@ public class FXProjectTemplate implements ActionListener {
 
     /**
      * Gets the name of the project template.
+     *
      * @return the name of the project template.
      */
     public String getName() {
@@ -51,6 +53,7 @@ public class FXProjectTemplate implements ActionListener {
 
     /**
      * Sets the name of the project template.
+     *
      * @param name the name of the project template.
      */
     public void setName(String name) {
@@ -59,38 +62,43 @@ public class FXProjectTemplate implements ActionListener {
 
     /**
      * Returns the location of the sourcefile for this project template.
+     *
      * @return the location of the sourcefile.
      */
-    public File getSourceFile() {
+    public Path getSourceFile() {
         return sourceFile;
     }
 
     /**
      * Sets the location of the sourcefile for this project template.
+     *
      * @param sourceFile the location of the source file.
      */
-    public void setSourceFile(File sourceFile) {
+    public void setSourceFile(Path sourceFile) {
         this.sourceFile = sourceFile;
     }
 
     /**
      * Returns the relative path for this template.
+     *
      * @return the relative path for the template.
      */
-    public String getRelativePath(){
+    public Path getRelativePath() {
         return relativePath;
     }
 
     /**
      * Sets the relative path for this template.
+     *
      * @param relativePath the new relative path for this template.
      */
-    public void setRelativePath(String relativePath){
+    public void setRelativePath(Path relativePath) {
         this.relativePath = relativePath;
     }
 
     /**
      * Returns the description for this FXProjectTemplate.
+     *
      * @return the description for the FXProjectTemplate.
      */
     public String getDescription() {
@@ -99,6 +107,7 @@ public class FXProjectTemplate implements ActionListener {
 
     /**
      * Sets the description for this FXProjectTemplate.
+     *
      * @param description the new description for the FXProjectTemplate.
      */
     public void setDescription(String description) {
@@ -107,6 +116,7 @@ public class FXProjectTemplate implements ActionListener {
 
     /**
      * Gets the UI Label for the FXProjectTemplate.
+     *
      * @return the label for the FXProjectTemplate.
      */
     public String getUILabel() {
@@ -115,6 +125,7 @@ public class FXProjectTemplate implements ActionListener {
 
     /**
      * Sets the label for the FXProjectTemplate.
+     *
      * @param uiLabel the new label for the FXProjectTemplate.
      */
     public void setUILabel(String uiLabel) {
@@ -122,22 +133,19 @@ public class FXProjectTemplate implements ActionListener {
     }
 
     /**
-     * Called when the user clicks on an action item that is connected with this
-     * FXProjectTemplate object.
-     * @param e the action event
-     */
-    public void actionPerformed(ActionEvent e) {
-        loadFile();
-    }
-
-    /**
      * Loads the file into a new project.
+     * @return returns a new FXProject instantiated from the given template.
      */
-    private void loadFile() {
+    public FXProject createNewProject() {
+        Path file = getSourceFile();
+        FXProjectLoader loader = new FXProjectLoader(file);
+        FXProject project = loader.load();
+        return project;
     }
 
     /**
-     *  Returns the group this template belongs to.
+     * Returns the group this template belongs to.
+     *
      * @return the group of this template
      */
     public String getGroup() {
@@ -146,6 +154,7 @@ public class FXProjectTemplate implements ActionListener {
 
     /**
      * Sets the group this template belongs to.
+     *
      * @param group the group of this template.
      */
     public void setGroup(String group) {
@@ -154,9 +163,16 @@ public class FXProjectTemplate implements ActionListener {
 
     /**
      * Checks if this FXProjectTemplate has a relative path setting.
-     * @return true if the FXProjectTemplate has relative path,false otherwise.
+     *
+     * @return true if the FXProjectTemplate has a relative path,false
+     * otherwise.
      */
     public boolean hasRelativePath() {
-        return relativePath != null || relativePath.length() == 0;
+        return relativePath != null && relativePath.getNameCount() > 0;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 }
