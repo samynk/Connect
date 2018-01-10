037149649ea7d4369c84ede7da240cf6f1d81aea
Koen Samyn <samyn.koen@gmail.com>
2018-01-10 10:13:08 +0100

    An action to create new projects from templates.

---
+++ src/main/java/dae/fxcreator/ui/actions/ProjectTemplateAction.java
@@ -0,0 +1,27 @@
+package dae.fxcreator.ui.actions;
+
+import dae.fxcreator.io.FXProjectTemplate;
+import dae.fxcreator.io.FXSingleton;
+import java.awt.event.ActionEvent;
+import javax.swing.AbstractAction;
+import javax.swing.Action;
+
+/**
+ * 
+ * @author Koen Samyn (samyn.koen@gmail.com)
+ */
+public class ProjectTemplateAction extends AbstractAction{
+    private final FXProjectTemplate template;
+    
+    public ProjectTemplateAction(FXProjectTemplate template){
+        super(template.getUILabel());
+        putValue(Action.SHORT_DESCRIPTION, template.getDescription());
+        this.template = template;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        NewProjectEvent npe = new NewProjectEvent(template);
+        FXSingleton.getSingleton().postEvent(npe);
+    }
+}
