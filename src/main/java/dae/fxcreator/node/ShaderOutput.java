845522f36031e345dfb785c7388fc7b000153ac7
Koen Samyn <samyn.koen@gmail.com>
2018-01-11 10:01:42 +0100

    moved functionality to super class. Added anchor feature.

--- src/main/java/dae/fxcreator/node/ShaderOutput.java
+++ src/main/java/dae/fxcreator/node/ShaderOutput.java
@@ -1,39 +1,22 @@
 package dae.fxcreator.node;
 
 import java.util.ArrayList;
-import javax.swing.event.ChangeEvent;
-import javax.swing.event.ChangeListener;
 
 /**
  *
  * @author Koen
  */
-public class ShaderOutput implements ShaderIO {
-
-    private String name;
-    private ShaderType type;
-    /**
-     * The parent node of this ShaderOutput node.
-     */
-    private IONode parent;
+public class ShaderOutput extends ShaderIO {
     /**
      * The nodes that are connected to this output.
      */
-    private ArrayList<ShaderInput> nodes = new ArrayList<ShaderInput>();
+    private final ArrayList<ShaderInput> nodes = new ArrayList<>();
     /**
      * This property allows for outputs that have an external source, such as
      * variables that are defined by semantics.
      */
     private String connectionString;
-    /**
-     * The semantic for this shader output. The semantic is useful for
-     * outputs of global nodes and input and output structs for shader stages.
-     */
-    private Semantic semantic = new Semantic();
-    /**
-     * The list of change listeners
-     */
-    private ArrayList<ChangeListener> listeners = new ArrayList<ChangeListener>();
+   
     /**
      * The type inference rule for this shader output object.
      */
@@ -47,10 +30,7 @@ public class ShaderOutput implements ShaderIO {
      * @param type the type of the ShaderOutput.
      */
     public ShaderOutput(IONode parent, String name, String semantic, String type, String typeRule) {
-        this.parent = parent;
-        this.type = parent.shaderTypeLib.getType(type);
-        this.name = name;
-        this.semantic.setValue(semantic);
+        super(parent,name, semantic, parent.shaderTypeLib.getType(type));
         this.typeRule = typeRule;
     }
 
@@ -62,24 +42,13 @@ public class ShaderOutput implements ShaderIO {
      * @param type the type of the ShaderOutput.
      */
     public ShaderOutput(IONode parent, String name, String semantic, ShaderType type, String typeRule) {
-        this.parent = parent;
-        this.type = type;
-        this.name = name;
-        this.semantic.setValue(semantic);
+        super(parent, name, semantic, type);
         this.typeRule = typeRule;
-    }
-    
-    /**
-     * Checks if the type of this input is an actual value.
-     * @return true if this input connects to a value type, false otherwise.
-     */
-    public boolean getValueType(){
-        return type != null && type.isValueType();
     }
 
     /**
      * Sets the type inference rule for this ShaderOutput object.
-     * @param rule the rule for this ShaderOutput object.
+     * @param typeRule the rule for this ShaderOutput object.
      */
     public void setTypeRule(String typeRule) {
         this.typeRule = typeRule;
@@ -94,58 +63,13 @@ public class ShaderOutput implements ShaderIO {
     }
 
     /**
-     * Returns the parent node for this shader.
-     * @return the parent node.
-     */
-    public IONode getParent() {
-        return parent;
-    }
-
-    /**
-     * Sets the parent node of this ShaderOutput object.
-     * @param node the node to set as parent.
-     */
-    public void setParent(IONode node) {
-        this.parent = node;
-    }
-
-    /**
-     * Returns the name for the ShaderOutput object.
-     * @return the name of the shader input object.
-     */
-    public String getName() {
-        return name;
-    }
-
-    /**
-     * Sets the name for the shader input object.
-     * @param name the name for the shader input object.
-     */
-    public void setName(String name) {
-        if (!this.name.equals(name)) {
-            String oldName = this.name;
-            this.name = name;
-            if (this.parent != null) {
-                parent.remapOutputName(oldName, this);
-            }
-        }
-    }
-
-    /**
-     * Returns the type for the shader input object.
-     * @return the type for the shader input object.
-     */
-    public ShaderType getType() {
-        return type;
-    }
-
-    /**
      * Sets the type for the shader input object.
      * @param type the type for the shader input object.
      */
+    @Override
     public void setType(ShaderType type) {
-        if (!type.equals(this.type)) {
-            this.type = type;
+        if (!type.equals(getType())) {
+            super.setType(type);
             notifyListeners();
 
             /**
@@ -177,26 +101,6 @@ public class ShaderOutput implements ShaderIO {
      */
     public void setConnectionString(String connectionString) {
         this.connectionString = connectionString;
-    }
-
-    /**
-     * Returns the semantic for this output object.
-     * @return the semantic for this output object.
-     */
-    public Semantic getSemantic() {
-        return semantic;
-    }
-
-    /**
-     * Sets the semantic for this output object.
-     * @param semantic the semantic for the output object.
-     */
-    public void setSemantic(String semantic) {
-        if (!semantic.equals(this.semantic.getValue())) {
-            String oldValue = this.semantic.getValue();
-            this.semantic.setValue(semantic);
-            parent.remapOutputSemantic(oldValue, this);
-        }
     }
 
     /**
@@ -237,24 +141,17 @@ public class ShaderOutput implements ShaderIO {
         return false;
     }
 
-    /**
-     * Returns a label for this ShaderInput object.
-     */
-    public String getLabel() {
-        if (semantic.isValid()) {
-            return this.name + " : " + semantic;
-        } else {
-            return this.name;
-        }
-    }
+
 
     /**
      * The reference of an input is created by asking the variable
      * name of the connected output port.
      * @return the variable name.
      */
+    @Override
     public String getRef() {
-        if (this.parent != null) {
+        IONode parent = getParent();
+        if (parent != null) {
             // if this is the only output, use the name of the parent id as
             // reference.
             if (parent.getNrOfOutputs() == 1 && !parent.isInputNode()) {
@@ -271,6 +168,7 @@ public class ShaderOutput implements ShaderIO {
      * Returns the variable name for this output object.
      * @return
      */
+    @Override
     public String getVar() {
         return getRef();
     }
@@ -279,6 +177,7 @@ public class ShaderOutput implements ShaderIO {
      * Checks if this ShaderIO object is connected.
      * @return true if the ShaderIO object is connected, false otherwise.
      */
+    @Override
     public boolean getConnected() {
         return this.nodes.size() > 0;
     }
@@ -287,6 +186,7 @@ public class ShaderOutput implements ShaderIO {
      * Checks if this is an input node.
      * @return true if this is an input node, false otherwise.
      */
+    @Override
     public boolean isInput() {
         return false;
     }
@@ -295,6 +195,7 @@ public class ShaderOutput implements ShaderIO {
      * Checks if this is an output node.
      * @return true if this is an output node, false otherwise.
      */
+    @Override
     public boolean isOutput() {
         return true;
     }
@@ -303,6 +204,7 @@ public class ShaderOutput implements ShaderIO {
      * Checks if this input or output is compatible with the provided shader io object.
      * @return true if this object is compatible (can connect) with the provided object.
      */
+    @Override
     public boolean accepts(ShaderIO io) {
         if (io.isInput()) {
             return io.accepts(this);
@@ -318,32 +220,6 @@ public class ShaderOutput implements ShaderIO {
         ArrayList<ShaderInput> inputsClone = (ArrayList) this.nodes.clone();
         for (ShaderInput input : inputsClone) {
             input.disconnectInput();
-        }
-    }
-
-    /**
-     * Adds a change listener to this io object.
-     * @param cl the change listener to add to the list of listeners.
-     */
-    public void addChangeListener(ChangeListener cl) {
-        listeners.add(cl);
-    }
-
-    /**
-     * Removes the change listener from this io objecT.
-     * @param cl the change listener to remove from the list of listeners.
-     */
-    public void removeChangeListener(ChangeListener cl) {
-        listeners.add(cl);
-    }
-
-    /**
-     * Notifies the listeners that this object has changed.
-     */
-    private void notifyListeners() {
-        ChangeEvent ce = new ChangeEvent(this);
-        for (ChangeListener cl : listeners) {
-            cl.stateChanged(ce);
         }
     }
 }
