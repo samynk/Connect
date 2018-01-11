b6dfccf6f839672d08195603d7930ea3af09522e
Koen Samyn <samyn.koen@gmail.com>
2018-01-11 09:58:29 +0100

    Code layout fixes. Clone now correctly clones the input and output anchors.

--- src/main/java/dae/fxcreator/node/ShaderNode.java
+++ src/main/java/dae/fxcreator/node/ShaderNode.java
@@ -6,12 +6,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * This class describes the general form for a shader node. A shader node
- * has inputs/output and settings.
+ * This class describes the general form for a shader node. A shader node has
+ * inputs/output and settings.
  *
  * @author Koen
  */
 public class ShaderNode extends IONode implements Cloneable {
+
     /**
      * The group type .
      */
@@ -22,16 +23,17 @@ public class ShaderNode extends IONode implements Cloneable {
     private String typeName;
 
     /**
-     * Creates a new ShaderNode object with an id, a name and a type. The
-     * id is used for code generation purposes, the name is used for user
-     * interface purposes.
+     * Creates a new ShaderNode object with an id, a name and a type. The id is
+     * used for code generation purposes, the name is used for user interface
+     * purposes.
+     *
      * @param id the id for the ShaderNode.
      * @param name the name for the ShaderNode.
      * @param type the type of node.
      * @param project the project this node is a part of.
      */
     public ShaderNode(String id, String name, String type, FXProject project) {
-        super(id, name,project);
+        super(id, name, project);
         super.setType(type);
         if (type != null) {
             int dotIndex = type.indexOf('.');
@@ -44,6 +46,7 @@ public class ShaderNode extends IONode implements Cloneable {
 
     /**
      * Returns the group of the node type
+     *
      * @return the group of the type of this node.
      */
     public String getTypeGroup() {
@@ -52,6 +55,7 @@ public class ShaderNode extends IONode implements Cloneable {
 
     /**
      * Returns the name of the node type without the group.
+     *
      * @return the name of type.
      */
     public String getTypeName() {
@@ -60,26 +64,28 @@ public class ShaderNode extends IONode implements Cloneable {
 
     @Override
     public Object clone() throws CloneNotSupportedException {
-        ShaderNode node = new ShaderNode(this.getId(), this.getName(), this.getType(),super.getFXProject());
+        ShaderNode node = new ShaderNode(this.getId(), this.getName(), this.getType(), super.getFXProject());
         node.setInputOutputEditable(this.isInputOutputEditable());
         node.setIcon(this.getIcon());
         // copy inputs
         List<ShaderInput> inputs = this.getInputs();
         for (ShaderInput input : inputs) {
-            ShaderInput copy = new ShaderInput(node, input.getName(), input.getSemantic().getValue(),input.getType(),input.getAcceptTypeSet());
+            ShaderInput copy = new ShaderInput(node, input.getName(), input.getSemantic().getValue(), input.getType(), input.getAcceptTypeSet());
             copy.setConnectionString(input.getConnectionString());
+            copy.setAnchor(input.getAnchor());
             node.addInput(copy);
         }
         List<ShaderOutput> outputs = this.getOutputs();
         for (ShaderOutput output : outputs) {
-            ShaderOutput copy = new ShaderOutput(node, output.getName(),output.getSemantic().getValue(), output.getType(),output.getTypeRule());
+            ShaderOutput copy = new ShaderOutput(node, output.getName(), output.getSemantic().getValue(), output.getType(), output.getTypeRule());
             copy.setConnectionString(output.getConnectionString());
+            copy.setAnchor(output.getAnchor());
             node.addOutput(copy);
         }
 
         List<ShaderInput> templateInputs = this.getTemplateInputs();
-        for ( ShaderInput input : templateInputs){
-            ShaderInput copy = new ShaderInput(node, input.getName(), input.getSemantic().getValue(),input.getType(),input.getAcceptTypeSet());
+        for (ShaderInput input : templateInputs) {
+            ShaderInput copy = new ShaderInput(node, input.getName(), input.getSemantic().getValue(), input.getType(), input.getAcceptTypeSet());
             copy.setConnectionString(input.getConnectionString());
             node.addTemplateInput(copy);
         }
@@ -91,6 +97,8 @@ public class ShaderNode extends IONode implements Cloneable {
                 node.addSetting(group.getName(), (Setting) setting.clone());
             }
         }
+        node.setInputAnchor( getInputAnchor() );
+        node.setOutputAnchor( getOutputAnchor() );
         return node;
     }
 
@@ -98,11 +106,9 @@ public class ShaderNode extends IONode implements Cloneable {
      * Remove all the outputs in this node.
      */
     public void removeOutputs() {
-        ArrayList<ShaderOutput> outputs = (ArrayList<ShaderOutput>)this.getOutputs().clone();
-        for(ShaderOutput output : outputs){
+        ArrayList<ShaderOutput> outputs = (ArrayList<ShaderOutput>) this.getOutputs().clone();
+        for (ShaderOutput output : outputs) {
             this.removeOutput(output);
         }
     }
-
-
 }
