88c5deaff80977b87db0690a177d459031db20a0
Koen Samyn <samyn.koen@gmail.com>
2018-01-16 17:33:32 +0100

    A class that distinguishes an ordinary string from a reference to an object.

---
+++ src/main/java/dae/fxcreator/io/codegen/parser/exec/VarID.java
@@ -0,0 +1,16 @@
+package dae.fxcreator.io.codegen.parser.exec;
+
+/**
+ *
+ * @author Koen Samyn (samyn.koen@gmail.com)
+ */
+public class VarID {
+    private String id;
+    public VarID(String id){
+        this.id = id;
+    }
+    
+    public String getId(){
+        return id;
+    }
+}
