487cd398f1821ab5b8070f57824a3ffea2b76239
Koen Samyn <samyn.koen@gmail.com>
2018-01-17 14:09:28 +0100

    FXProject is loaded from a template to facilitate testing all aspects of an export.

--- src/test/java/dae/fxcreator/io/codegen/parser/VisitorTest.java
+++ src/test/java/dae/fxcreator/io/codegen/parser/VisitorTest.java
@@ -1,11 +1,20 @@
 package dae.fxcreator.io.codegen.parser;
 
 import dae.fxcreator.io.FXProject;
+import dae.fxcreator.io.FXProjectTemplate;
+import dae.fxcreator.io.FXProjectTemplates;
+import dae.fxcreator.io.FXProjectType;
+import dae.fxcreator.io.FXSettings;
+import dae.fxcreator.io.FXSingleton;
 import dae.fxcreator.io.PathUtil;
 import dae.fxcreator.io.ShaderStage;
 import dae.fxcreator.io.codegen.ExportTask;
+import dae.fxcreator.io.loaders.FXProjectTemplateLoader;
+import dae.fxcreator.io.loaders.FXProjectTypeLoader;
+import dae.fxcreator.io.loaders.FXSettingLoader;
 import java.io.IOException;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.antlr.v4.runtime.CharStream;
@@ -25,22 +34,39 @@ import org.junit.Test;
  * @author Koen Samyn (samyn.koen@gmail.com)
  */
 public class VisitorTest {
-    
+
+    FXProjectTemplate basic;
+
     public VisitorTest() {
     }
-    
+
     @BeforeClass
     public static void setUpClass() {
     }
-    
+
     @AfterClass
     public static void tearDownClass() {
     }
-    
+
     @Before
     public void setUp() {
+        FXProjectTypeLoader loader = new FXProjectTypeLoader("conf/fxcreator.json");
+        loader.load();
+        ArrayList<FXProjectType> projectTypes = loader.getProjectTypes();
+        
+        FXSingleton.getSingleton().setSupportedProjectTypes(loader.getProjectTypes());
+
+        for (FXProjectType projectType : projectTypes) {
+            if ("rigging".equals(projectType.getName())) {
+                String templatesFile = projectType.getTemplates();
+                FXProjectTemplateLoader tLoader = new FXProjectTemplateLoader(templatesFile);
+                FXProjectTemplates ft = tLoader.load();
+
+                basic = ft.getTemplate("basic", "basic");
+            }
+        }
     }
-    
+
     @After
     public void tearDown() {
     }
@@ -52,34 +78,33 @@ public class VisitorTest {
             GraphTransformerLexer lexer = new GraphTransformerLexer(charStream);
             TokenStream tokens = new CommonTokenStream(lexer);
             GraphTransformerParser parser = new GraphTransformerParser(tokens);
-            
+
             Visitor classVisitor = new Visitor();
             Object traverseResult = classVisitor.visit(parser.transform());
-            
+
             int syntaxErrors = parser.getNumberOfSyntaxErrors();
-            Assert.assertEquals("There were syntax errors", 0,syntaxErrors);
+            Assert.assertEquals("There were syntax errors", 0, syntaxErrors);
+
+            Assert.assertTrue(traverseResult instanceof TemplateClassLibrary);
+
+            TemplateClassLibrary tcl = (TemplateClassLibrary) traverseResult;
+
+            Assert.assertNotNull("Rigging project template is null", basic);
             
-            Assert.assertTrue( traverseResult instanceof TemplateClassLibrary);
-            
-            TemplateClassLibrary tcl = (TemplateClassLibrary)traverseResult;
-            
-            FXProject testProject = new FXProject(Paths.get("/test/test.fxpoj"));
+            FXProject testProject = basic.createNewProject();
             testProject.setName("testProject1");
-            
-            testProject.addShaderStage(new ShaderStage("vertexStage","vertex"));
-            testProject.addShaderStage(new ShaderStage("pixelStage","pixel"));
-            
-            ExportTask et = new ExportTask(testProject,PathUtil.createUserDirPath("/test/test.rig"),tcl);
+
+            ExportTask et = new ExportTask(testProject, PathUtil.createUserDirPath("/test/test.rig"), tcl);
             et.export();
-            
+
             StringBuilder sb = et.getCodeOutput().getBuffer("default");
-            Assert.assertTrue("default buffer should have content.",sb.length() > 0 );
-            
+            Assert.assertTrue("default buffer should have content.", sb.length() > 0);
+
             System.out.println("The contents of the buffer :");
             System.out.println("----------------------------");
             System.out.println(sb.toString());
             System.out.println("----------------------------");
-            
+
         } catch (IOException ex) {
             Logger.getLogger(VisitorTest.class.getName()).log(Level.SEVERE, null, ex);
         }
