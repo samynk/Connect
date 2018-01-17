577fa6eaad7588dddfa1f9bc91bfc14b0cc434e5
Koen Samyn <samyn.koen@gmail.com>
2018-01-17 14:08:01 +0100

    Implementation of writing a setting to the output.

---
+++ src/main/java/dae/fxcreator/io/codegen/parser/exec/WriteSetting.java
@@ -0,0 +1,39 @@
+package dae.fxcreator.io.codegen.parser.exec;
+
+import dae.fxcreator.io.codegen.ExportTask;
+import dae.fxcreator.io.templates.Setting;
+import dae.fxcreator.node.IONode;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Koen Samyn (samyn.koen@gmail.com)
+ */
+public class WriteSetting implements Executable {
+
+    private final String object;
+    private final String settingGroup;
+    private final String settingProperty;
+
+    public WriteSetting(String object, String group, String property) {
+        this.object = object;
+        this.settingGroup = group;
+        this.settingProperty = property;
+    }
+
+    @Override
+    public void execute(Object codeObject, ExportTask context) {
+        Object o = context.getVar(object);
+        if (o == null && !(o instanceof IONode)) {
+            return;
+        }
+        IONode node = (IONode) o;
+        Setting value = node.getSetting(settingGroup, settingProperty);
+        if (value != null) {
+            context.peekBuffer().append(value.toString());
+        }
+    }
+}
