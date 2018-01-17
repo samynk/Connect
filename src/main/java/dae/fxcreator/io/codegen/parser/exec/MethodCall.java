670b29034ed52f367e9baeb05052ca95a864b74a
Koen Samyn <samyn.koen@gmail.com>
2018-01-17 15:43:35 +0100

    Changed implementation to procedural method calls.

--- src/main/java/dae/fxcreator/io/codegen/parser/exec/MethodCall.java
+++ src/main/java/dae/fxcreator/io/codegen/parser/exec/MethodCall.java
@@ -7,28 +7,32 @@ import dae.fxcreator.io.codegen.ExportTask;
  *
  * @author Koen Samyn (samyn.koen@gmail.com)
  */
-public class MethodCall implements Executable{
-    private final String object;
+public class MethodCall implements Executable {
+
     private final String method;
     private final Object[] parameters;
-    
-    public MethodCall(String object, String method, Object ... parameters){
-        this.object = object;
+
+    public MethodCall(String method, Object... parameters) {
         this.method = method;
         this.parameters = parameters;
     }
 
     @Override
     public void execute(Object codeObject, ExportTask context) {
-        Object o = context.getVar(object);
-        if ( "call".equals(method))
-        {
-            if ( o instanceof TypedNode && parameters[0] instanceof String){
-                context.template((TypedNode)o,(String)parameters[0]);
-                // restore the node parameter
-                context.addVar("node", codeObject);
+
+        if ("call".equals(method)) {
+            if (parameters[0] instanceof VarID && parameters[1] instanceof String) {
+                Object o = context.getVar(parameters[0].toString());
+                if (o instanceof TypedNode) {
+                    context.template((TypedNode) o, (String) parameters[1]);
+                    // restore the node parameter
+                    context.addVar("node", codeObject);
+                }
+            }
+        }else if ("writeBufferToStream".equals(method)){
+            if (parameters[0] instanceof String && parameters[1] instanceof String) {
+                context.writeBufferToStream(parameters[0].toString(), parameters[1].toString());
             }
         }
     }
-
 }
