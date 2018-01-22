1ae16fd83944712e10186288bde21ea45b87200e
Koen Samyn <samyn.koen@gmail.com>
2018-01-22 13:43:46 +0100

    Layout and documentation fixes.

--- src/main/java/dae/fxcreator/node/StructListener.java
+++ src/main/java/dae/fxcreator/node/StructListener.java
@@ -2,28 +2,39 @@ package dae.fxcreator.node;
 
 /**
  * This interface makes it possible to listen to struct changes.
- * @author Koen
+ *
+ * @author Koen Samyn (samyn.koen@gmail.com)
  */
 public interface StructListener {
+
     /**
      * This method is called when the field of a struct was changed.
+     *
      * @param struct the struct that was changed.
      * @param field the field that was changed.
+     * @param oldValue the old value for the field.
      */
-    public void structFieldUpdated(ShaderStruct struct,ShaderField field, ShaderField oldValue);
+    public void structFieldUpdated(ShaderStruct struct, ShaderField field, ShaderField oldValue);
 
     /**
      * This method is called when a new field is inserted in the struct.
+     *
+     * @param struct the struct where a field was inserted.
      * @param field the field that was inserted.
      */
-    public void structFieldInserted(ShaderStruct struct,ShaderField field);
+    public void structFieldInserted(ShaderStruct struct, ShaderField field);
+
     /**
      * This method is called when a field was remove from the struct.
+     *
+     * @param struct the struct where a field was removed.
      * @param field the field that was removed.
      */
     public void structFieldRemoved(ShaderStruct struct, ShaderField field);
+
     /**
      * This method is called when the id of the struct was changed.
+     *
      * @param struct the struct whose id was changed.
      */
     public void structIdChanged(ShaderStruct struct);
