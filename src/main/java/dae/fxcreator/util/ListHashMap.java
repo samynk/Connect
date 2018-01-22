88a378449769fe25d3cc9a0713e93a8229d5d2ee
Koen Samyn <samyn.koen@gmail.com>
2018-01-22 13:45:07 +0100

    Added convenience methods.

--- src/main/java/dae/fxcreator/util/ListHashMap.java
+++ src/main/java/dae/fxcreator/util/ListHashMap.java
@@ -1,8 +1,13 @@
 package dae.fxcreator.util;
 
+import dae.fxcreator.io.NodeGroup;
+import dae.fxcreator.io.ShaderStage;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.List;
+import javax.swing.tree.TreeNode;
 
 /**
  * A combination of a hashmap and a list.
@@ -22,45 +27,108 @@ public class ListHashMap<T extends Key> {
     public ListHashMap() {
 
     }
+    
+    /**
+     * Returns the size of the items.
+     * @return the size of the items.
+     */
+    public int size(){
+        return items.size();
+    }
 
     /**
      * Adds an item to this collection.
+     *
      * @param item the item to add.
      */
     public void addItem(T item) {
         items.add(item);
         itemMap.put(item.getKey(), item);
     }
-    
+
     /**
      * Remove the item from the collection.
+     *
      * @param item the item to remove.
      * @return true if the item was removed, false otherwise.
      */
-    public boolean removeItem(T item){
+    public boolean removeItem(T item) {
         boolean removed = items.remove(item);
-        if ( removed ){
+        if (removed) {
             itemMap.remove(item.getKey());
         }
         return removed;
     }
-    
+
     /**
      * Add all the items in the list to this collection.
+     *
      * @param items a collection of T objects.
      */
-    public void addAll(List<T> items){
+    public void addAll(List<T> items) {
         items.forEach((item) -> {
             addItem(item);
         });
     }
-    
+
     /**
      * Find the object with the given key.
+     *
      * @param key the key of the object.
      * @return the object with the given key.
      */
-    public T find(String key){
+    public T find(String key) {
         return itemMap.get(key);
+    }
+
+    /**
+     * Return the first item.
+     * @return the first item.
+     */
+    public T first() {
+        return items.get(0);
+    }
+    
+    /**
+     * Return the item with the given index.
+     * @param index the index into the list.
+     * @return the object of type T
+     */
+    public T getItem(int index){
+        return items.get(index);
+    }
+
+    /**
+     * Check if the key is present in the hash map.
+     * @param name the name of the key.
+     * @return true if the key is in the hash map, false otherwise.
+     */
+    public boolean containsKey(String name) {
+        return itemMap.containsKey(name);
+    }
+
+    /**
+     * Returns an iterable to iterate over the items.
+     * @return the iterable.
+     */
+    public Iterable<T> items() {
+        return items;
+    }
+
+    /**
+     * Returns the index of the item.
+     * @param item the item to get the index of.
+     * @return the index of the item, or -1 if no item was found.
+     */
+    public int indexOf(T item) {
+        return items.indexOf(item);
+    }
+
+    /**
+     * Returns this collection as an enumeration.
+     * @return the list as an Enumeration object.
+     */
+    public Enumeration enumeration() {
+        return Collections.enumeration(items);
     }
 }
