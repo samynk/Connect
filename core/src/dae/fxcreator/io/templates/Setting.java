9b2d198daaea7f240f7ee0c667f537cd58007daa
Koen Samyn <samyn.koen@gmail.com>
2018-01-23 13:29:46 +0100

    Documentation and code layout changes.

--- core/src/dae/fxcreator/io/templates/Setting.java
+++ core/src/dae/fxcreator/io/templates/Setting.java
@@ -7,6 +7,7 @@ import java.util.Iterator;
 
 /**
  * General interface for the settings for a shader node.
+ *
  * @author Koen
  */
 public abstract class Setting implements Cloneable, SymbolListener {
@@ -23,7 +24,7 @@ public abstract class Setting implements Cloneable, SymbolListener {
     /**
      * The list of globals this option setting should listen for.
      */
-    private ArrayList<String> globalSymbols = new ArrayList<String>();
+    private ArrayList<String> globalSymbols = new ArrayList<>();
     /**
      * The old value for the setting.
      */
@@ -35,8 +36,8 @@ public abstract class Setting implements Cloneable, SymbolListener {
     }
 
     /**
-     * Notifies the node object that the setting was changed.
-     * checks if the node != null before calling the node method.
+     * Notifies the node object that the setting was changed. checks if the node
+     * != null before calling the node method.
      */
     protected void notifySettingChanged() {
         if (node != null) {
@@ -46,6 +47,7 @@ public abstract class Setting implements Cloneable, SymbolListener {
 
     /**
      * Returns the label for this setting.
+     *
      * @return the label for this setting.
      */
     public String getLabel() {
@@ -54,6 +56,7 @@ public abstract class Setting implements Cloneable, SymbolListener {
 
     /**
      * Sets the label for this setting.
+     *
      * @param label the label for this setting.
      */
     public void setLabel(String label) {
@@ -62,6 +65,7 @@ public abstract class Setting implements Cloneable, SymbolListener {
 
     /**
      * Returns the id for this setting.
+     *
      * @return the id of the setting
      */
     public String getId() {
@@ -70,14 +74,16 @@ public abstract class Setting implements Cloneable, SymbolListener {
 
     /**
      * Sets the id for this setting.
+     *
      * @param id the new id for the setting.
      */
-    public void setId(String name) {
-        this.id = name;
+    public void setId(String id) {
+        this.id = id;
     }
 
     /**
      * Gets the IONode this setting belongs to.
+     *
      * @return the IONode this setting is for.
      */
     public IONode getSettingNode() {
@@ -86,7 +92,8 @@ public abstract class Setting implements Cloneable, SymbolListener {
 
     /**
      * Sets the IONode this setting is for.
-     * @param IONode the node that is bound to this setting.
+     *
+     * @param node the node that is bound to this setting.
      */
     public void setSettingNode(IONode node) {
         this.node = node;
@@ -94,6 +101,10 @@ public abstract class Setting implements Cloneable, SymbolListener {
 
     /**
      * Creates a clone of this settings object.
+     *
+     * @return the cloned setting.
+     * @throws java.lang.CloneNotSupportedException thrown if the object does
+     * not support cloning.
      */
     @Override
     public Object clone() throws CloneNotSupportedException {
@@ -102,14 +113,16 @@ public abstract class Setting implements Cloneable, SymbolListener {
 
     /**
      * Sets the group this setting belongs to.
-     * @param id the id of the group.
+     *
+     * @param group the id of the group.
      */
-    public void setGroup(String name) {
-        this.group = name;
+    public void setGroup(String group) {
+        this.group = group;
     }
 
     /**
      * Returns the group this setting belongs to.
+     *
      * @return the group this setting belongs to.
      */
     public String getGroup() {
@@ -118,44 +131,52 @@ public abstract class Setting implements Cloneable, SymbolListener {
 
     /**
      * Returns the type of the Setting.
+     *
      * @return the type of setting.
      */
     public abstract String getType();
 
     /**
      * Returns the value of the Setting as a String.
+     *
      * @return the value of this setting as a string.
      */
     public abstract String getSettingValue();
+
     /**
      * Returns the value of this Setting as a formatted value for use in a
      * shader.
+     *
      * @return the value of this setting as a formatted value
      */
     public abstract String getFormattedValue();
 
     /**
      * Returns the value of the Setting as an Object.
+     *
      * @return the value of the Setting as an Object.
      */
     public abstract Object getSettingValueAsObject();
+
     /**
      * Sets the setting value of this setting as an object.
-     * @param the new value for this setting.
+     *
+     * @param o the new value for this setting.
      */
     public abstract void setSettingValueAsObject(Object o);
 
     /**
      * Sets the value for this Setting as a String.
+     *
      * @param value the value for this setting as a string.
      */
     public abstract void setSettingValue(String value);
 
-
     /**
-     * Checks if the value of the setting is written as an attribute (with
-     * the name value) or as a cdata section inside a value element. (default
-     * is to write as an attribute).
+     * Checks if the value of the setting is written as an attribute (with the
+     * name value) or as a cdata section inside a value element. (default is to
+     * write as an attribute).
+     *
      * @return the writeValueAsAttribute
      */
     public boolean isWriteValueAsAttribute() {
@@ -166,6 +187,7 @@ public abstract class Setting implements Cloneable, SymbolListener {
      * If the value should be written as an attribute, pass true to this method
      * otherwise, pass false to write the value as a cdata section inside the
      * setting.
+     *
      * @param writeValueAsAttribute the writeValueAsAttribute to set
      */
     public void setWriteValueAsAttribute(boolean writeValueAsAttribute) {
@@ -174,18 +196,19 @@ public abstract class Setting implements Cloneable, SymbolListener {
 
     /**
      * Check if the setting should be visualized in the node.
-     * @return the visualized state (true if the setting should be visualized
-     * in the node).
+     *
+     * @return the visualized state (true if the setting should be visualized in
+     * the node).
      */
     public boolean isVisualized() {
         return visualize;
     }
 
     /**
-     * Set the state of the visualization. If set to true , this setting will
-     * be visualized in the node.
-     * The default is false.
-     * @param visualize the visualize to set
+     * Set the state of the visualization. If set to true , this setting will be
+     * visualized in the node. The default is false.
+     *
+     * @param visualized true if the visual should be shown, false otherwise.
      */
     public void setVisualized(boolean visualized) {
         this.visualize = visualized;
@@ -193,6 +216,7 @@ public abstract class Setting implements Cloneable, SymbolListener {
 
     /**
      * Checks if the label if this setting is visible for a visualizer.
+     *
      * @return the labelVisible
      */
     public boolean isLabelVisible() {
@@ -201,6 +225,7 @@ public abstract class Setting implements Cloneable, SymbolListener {
 
     /**
      * Sets the label visibility for the visualizer.
+     *
      * @param labelVisible the labelVisible to set
      */
     public void setLabelVisible(boolean labelVisible) {
@@ -209,6 +234,7 @@ public abstract class Setting implements Cloneable, SymbolListener {
 
     /**
      * Returns the previous value for the setting.
+     *
      * @return the oldValue
      */
     public Object getOldValue() {
@@ -217,6 +243,7 @@ public abstract class Setting implements Cloneable, SymbolListener {
 
     /**
      * Sets the old value for the setting.
+     *
      * @param oldValue the oldValue to set
      */
     public void setOldValue(Object oldValue) {
@@ -225,6 +252,7 @@ public abstract class Setting implements Cloneable, SymbolListener {
 
     /**
      * Add a listener for the symbol. (possible values are currently STRUCT)
+     *
      * @param string the symbol to listen for in this setting.
      */
     public void addListenerFor(String string) {
@@ -233,44 +261,52 @@ public abstract class Setting implements Cloneable, SymbolListener {
 
     /**
      * Returns the symbols this listener wants to to listen for.
+     *
      * @return the keys this object wants to listen for.
      */
+    @Override
     public Iterator<String> getListenerSymbols() {
         return globalSymbols.iterator();
     }
 
     /**
      * Checks if this setting listens to the specific symbol.
+     *
      * @param type the symbol.
      * @return true if this setting listens for the specific symbol changes.
      */
-     public boolean listensToSymbol(String type) {
+    public boolean listensToSymbol(String type) {
         return globalSymbols.contains(type);
     }
 
     /**
      * Notifies that a symbol was added to the project. A symbol can be a
      * ShaderStage, Struct, Technique or Pass.
+     *
      * @param type the type of symbol that was added.
      * @param symbol the symbol that was added.
      */
+    @Override
     public void symbolAdded(String type, Object symbol) {
         throw new UnsupportedOperationException("Not supported yet.");
     }
 
     /**
-     * Notifies that a symbol was removed from the project. A symbol can be
-     * a ShaderStage, Struct, Techniqu or Pass.
+     * Notifies that a symbol was removed from the project. A symbol can be a
+     * ShaderStage, Struct, Techniqu or Pass.
+     *
      * @param type the type of symbol that was removed.
      * @param symbol the symbol that was removed.
      */
+    @Override
     public void symbolRemoved(String type, Object symbol) {
         throw new UnsupportedOperationException("Not supported yet.");
     }
 
     /**
-     * Checks if this setting is a default value. Writers can use this to skip settings
-     * that have default values.
+     * Checks if this setting is a default value. Writers can use this to skip
+     * settings that have default values.
+     *
      * @return true if the setting is set to a default, false otherwise.
      */
     public boolean isDefaultValue() {
@@ -279,7 +315,9 @@ public abstract class Setting implements Cloneable, SymbolListener {
 
     /**
      * Marks this setting as a default value.
-     * @param defaultValue pass true if this setting is a default value, false otheriwse.
+     *
+     * @param defaultValue pass true if this setting is a default value, false
+     * otheriwse.
      */
     public void setDefaultValue(boolean defaultValue) {
         this.defaultValue = defaultValue;
@@ -287,6 +325,7 @@ public abstract class Setting implements Cloneable, SymbolListener {
 
     /**
      * The value of the setting consists of well formed xml elements.
+     *
      * @param valueAsXML true if the value is an xml node, false otherwise.
      */
     public void setValueAsXML(boolean valueAsXML) {
@@ -295,9 +334,10 @@ public abstract class Setting implements Cloneable, SymbolListener {
 
     /**
      * Checks if the value is written as an xml element.
+     *
      * @return true if the value is an xml element, false otherwise.
      */
-    public boolean isValueAsXML(){
+    public boolean isValueAsXML() {
         return valueAsXML;
     }
 }
