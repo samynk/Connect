6d7bc7cd315838f857a23f42d2d87f5425183d6e
Koen Samyn <samyn.koen@gmail.com>
2018-01-25 13:46:15 +0100

    Moved PathUtil to io project and another package.

--- src/main/java/dae/fxcreator/ui/FXCreator2.java
+++ src/main/java/dae/fxcreator/ui/FXCreator2.java
@@ -8,7 +8,7 @@ import dae.fxcreator.io.templates.FXProjectTemplates;
 import dae.fxcreator.node.project.FXProjectType;
 import dae.fxcreator.gui.model.FXSettings;
 import dae.fxcreator.io.FXSingleton;
-import dae.fxcreator.io.PathUtil;
+import dae.fxcreator.io.util.PathUtil;
 import dae.fxcreator.node.transform.ExportTask;
 import dae.fxcreator.node.transform.TemplateClassLibrary;
 import dae.fxcreator.io.loaders.FXProjectTemplateLoader;
