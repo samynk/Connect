6158b2ffffda2db80745a86e4b1279a69d8b8212
Koen Samyn <samyn.koen@gmail.com>
2018-02-09 17:09:22 +0100

    Annotation to indicate a method that is available in the script interface.

---
+++ core/src/main/java/dae/fxcreator/node/annotations/Export.java
@@ -0,0 +1,16 @@
+package dae.fxcreator.node.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ *
+ * @author Koen Samyn (samyn.koen@gmail.com)
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface Export {
+    String name();
+}
