f53cdbf9eb94c2e8c1cea409fc8148744efc6e10
Koen Samyn <samyn.koen@gmail.com>
2018-02-09 17:09:47 +0100

    Removed unnecessary comment.

--- core/src/main/java/dae/fxcreator/node/transform/exec/Expression.java
+++ core/src/main/java/dae/fxcreator/node/transform/exec/Expression.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package dae.fxcreator.node.transform.exec;
 
 import dae.fxcreator.node.transform.ExportTask;
