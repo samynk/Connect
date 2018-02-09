89ea5dde466599fbb7bcb24c344984e4e540ef63
Koen Samyn <samyn.koen@gmail.com>
2018-02-09 17:04:32 +0100

    Object that can query an input or ouptut port from a node.

---
+++ core/src/main/java/dae/fxcreator/node/transform/exec/ObjectPortExpression.java
@@ -0,0 +1,36 @@
+package dae.fxcreator.node.transform.exec;
+
+import dae.fxcreator.node.IONode;
+import dae.fxcreator.node.ShaderIO;
+import dae.fxcreator.node.transform.ExportTask;
+
+/**
+ *
+ * @author Koen Samyn (samyn.koen@gmail.com)
+ */
+public class ObjectPortExpression implements Expression {
+    private final String portName;
+    private final boolean root;
+    
+    public ObjectPortExpression(String portName, boolean root){
+        this.portName = portName;
+        this.root = root;
+    }
+    
+    @Override
+    public Object evaluate(ExportTask context) {
+        Object top;
+        if ( root ){
+            top = context.getVar("node");
+        }else{
+            top = context.popChainStack();
+        }
+        if ( top instanceof IONode){
+            IONode node = (IONode)top;
+            ShaderIO port = node.getPort(portName);
+            return port;
+        }else{
+            return null;
+        }
+    }
+}
