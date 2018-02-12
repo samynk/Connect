ca0f2a657f11974236e0d989ac42536e90675833
Koen Samyn <samyn.koen@gmail.com>
2018-02-12 11:25:44 +0100

    Added documentation.

--- core/src/main/java/dae/fxcreator/node/graphmath/MathElement.java
+++ core/src/main/java/dae/fxcreator/node/graphmath/MathElement.java
@@ -6,18 +6,37 @@ import java.awt.Dimension;
 import java.awt.Graphics2D;
 
 /**
- *
- * @author Koen
+ * A math element that can be used to build complex mathematical
+ * instructions.
+ * @author Koen Samyn (samyn.koen@gmail.com)
  */
 public abstract class MathElement implements TypedNode{
-    protected Dimension size = new Dimension();
+    /**
+     * the size of the math element.
+     */
+    protected final Dimension size = new Dimension();
+    /**
+     * The baseline of the math element, useful for rendering.
+     */
     protected int baseLine;
+    /**
+     * Indicates if the math element can be changed, true if the
+     * math element can be changed.
+     */
     private boolean locked = false;
     
+    /**
+     * Returns true if the element does not accept child elements.
+     * @return true if the element is a leaf, false otherwise.
+     */
     public boolean isLeaf(){
         return true;
     }
     
+    /**
+     * Checks if this element can be changed.
+     * @return true if the element can be changed, false otherwise.
+     */
     public boolean isLocked(){
         return locked;
     }
@@ -46,6 +65,4 @@ public abstract class MathElement implements TypedNode{
     public abstract void render(Graphics2D g2d, int x, int y, int width, int height);
 
     public abstract Dimension calculateSize(Graphics2D g2d);
-
-    //public abstract void build(StringBuilder result, MathFormulaCodeGenerator codeGenerator);
 }
