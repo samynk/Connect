242574e39a054e2ca34d2d40bdb95b084ba6087c
Koen Samyn <samyn.koen@gmail.com>
2018-02-12 08:59:51 +0100

    Swapped out matheditor for simple text editor.

--- src/main/java/dae/fxcreator/node/graph/uisetting/MathSettingComponent.java
+++ src/main/java/dae/fxcreator/node/graph/uisetting/MathSettingComponent.java
@@ -2,47 +2,53 @@ package dae.fxcreator.node.graph.uisetting;
 
 import dae.fxcreator.node.events.IOListener;
 import dae.fxcreator.node.settings.MathSetting;
-import dae.fxcreator.node.IONode;
-import dae.fxcreator.node.ShaderInput;
-import dae.fxcreator.node.ShaderOutput;
-import dae.fxcreator.node.graph.math.MathEditor;
-import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import javax.swing.JEditorPane;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.event.ChangeEvent;
-import javax.swing.event.ChangeListener;
+import javax.swing.JScrollPane;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+import javax.swing.text.PlainDocument;
 
 /**
  * Allows the user to create a math formula.
  * @author Koen
  */
-public class MathSettingComponent extends JPanel implements ChangeListener, IOListener {
+public class MathSettingComponent extends JPanel implements IOListener, DocumentListener {
     //private MathSetting setting;
 
     private JLabel lblLabel;
-    private MathEditor txtMath;
+    private JEditorPane txtMath;
     private MathSetting setting;
 
     public MathSettingComponent() {
-        lblLabel = new JLabel("Formulas : ");
+        initComponents();
+    }
+
+    private void initComponents() {
+        lblLabel = new JLabel(" : ");
         setLayout(new GridBagLayout());
-        txtMath = new MathEditor();
-        txtMath.setPreferredSize(new Dimension(400, 300));
+        txtMath = new JEditorPane();
 
         GridBagConstraints gbc = new GridBagConstraints();
         gbc.gridx = 0;
+        gbc.fill = GridBagConstraints.HORIZONTAL;
         gbc.gridy = 0;
         gbc.weightx = 1.0;
+        gbc.weighty = 0;
+        add(lblLabel, gbc);
+        gbc.gridx = 0;
+        gbc.gridy = 1;
         gbc.weighty = 1.0;
         gbc.ipadx = 100;
         gbc.ipady = 50;
         gbc.fill = GridBagConstraints.BOTH;
+        add(new JScrollPane(txtMath), gbc);
 
-        add(txtMath, gbc);
-
-        txtMath.addChangeListener(this);
+        txtMath.getDocument().addDocumentListener(this);
+        txtMath.getDocument().putProperty(PlainDocument.tabSizeAttribute, 2);
     }
 
     void setMathSetting(MathSetting mathSetting) {
@@ -52,25 +58,19 @@ public class MathSettingComponent extends JPanel implements ChangeListener, IOLi
         if (mathSetting != null) {
             mathSetting.getSettingNode().addIOListener(this);
             this.setting = mathSetting;
-            txtMath.setMathFormula(setting.getMathFormula());
-            updateIONames();
+            txtMath.setText(setting.getSettingValue());
+            //updateIONames();
         }
     }
 
     @Override
     public void removeNotify() {
-        txtMath.removeChangeListener(this);
         if ( setting != null ){
             setting.getSettingNode().removeIOListener(this);
         }
     }
 
-    public void stateChanged(ChangeEvent e) {
-        setting.setSettingValueAsObject(txtMath.getMathFormula());
-        setting.setSettingValue(txtMath.getAsXML());
-        //setting.setIcon(txtMath.getSnapShot());
-    }
-
+    /*
     private void updateIONames() {
         if (setting == null) {
             return;
@@ -90,17 +90,40 @@ public class MathSettingComponent extends JPanel implements ChangeListener, IOLi
             ++i;
         }
         txtMath.addOutputVariables(outputs);
-    }
+    }*/
 
+    @Override
     public void ioChanged(String oldName, String newName) {
-        updateIONames();
+        // updateIONames();
     }
 
+    @Override
     public void ioRemoved(String name) {
-        updateIONames();
+        // updateIONames();
     }
 
+    @Override
     public void ioAdded(String name) {
-        updateIONames();
+        // updateIONames();
+    }
+    
+    @Override
+    public void changedUpdate(DocumentEvent e) {
+        updateSetting();
+    }
+
+    @Override
+    public void insertUpdate(DocumentEvent e) {
+        updateSetting();
+    }
+
+    @Override
+    public void removeUpdate(DocumentEvent e) {
+        updateSetting();
+    }
+    
+    private void updateSetting() {
+        String text = txtMath.getText();
+        setting.setSettingValue(text);
     }
 }
