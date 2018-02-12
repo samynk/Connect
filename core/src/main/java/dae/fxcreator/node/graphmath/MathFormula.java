3272d0a99a2d4a58dfda49085b66e3b4c473c64a
Koen Samyn <samyn.koen@gmail.com>
2018-02-12 08:58:24 +0100

    Fixed rendering bug. Fixed documentation layout.

--- core/src/main/java/dae/fxcreator/node/graphmath/MathFormula.java
+++ core/src/main/java/dae/fxcreator/node/graphmath/MathFormula.java
@@ -8,12 +8,12 @@ import java.util.ArrayList;
 
 /**
  * The root node for a math formula.
+ *
  * @author Koen
  */
 public class MathFormula extends MathElement implements TypedNode {
 
     private final ArrayList<MathElement> roots = new ArrayList<>();
-    
 
     public MathFormula() {
     }
@@ -24,37 +24,41 @@ public class MathFormula extends MathElement implements TypedNode {
 
     /**
      * Returns the root element.
+     *
      * @return
      */
     public Iterable<MathElement> getRoots() {
         return roots;
     }
-    
+
     /**
      * Returns the number of roots.
+     *
      * @return the number of roots.
      */
-    public int getNrOfRoots(){
+    public int getNrOfRoots() {
         return roots.size();
     }
 
     /**
      * Sets the root element for this MathFormula.
+     *
      * @param root the root element for the formula.
      */
     public void addRoot(MathElement root) {
         roots.add(root);
     }
-    
+
     /**
      * Returns the MathElement with the given index.
+     *
      * @param index the index of the element.
      * @return a MathElement at the given index, or null if it does not exist.
      */
-    public MathElement getRoot(int index){
-        if ( index < roots.size()){
+    public MathElement getRoot(int index) {
+        if (index < roots.size()) {
             return roots.get(index);
-        }else{
+        } else {
             return null;
         }
     }
@@ -72,34 +76,33 @@ public class MathFormula extends MathElement implements TypedNode {
     @Override
     public Dimension calculateSize(Graphics2D g2d) {
         // mathformula is vertically oriented.
-        size.setSize(0,0);
-        for ( MathElement element: roots)
-        {
+        size.setSize(0, 0);
+        for (MathElement element : roots) {
             Dimension eSize = element.calculateSize(g2d);
-            if ( eSize.width > size.width){
+            if (eSize.width > size.width) {
                 size.width = eSize.width;
-                size.height += eSize.height;
             }
+            size.height += eSize.height;
         }
+        size.width+=4;
         return size;
     }
 
     @Override
     public void render(Graphics2D g2d, int x, int y, int width, int height) {
-        
+
         // mathformula is vertically oriented.
-        for ( MathElement element: roots)
-        {
-            int offsetX = (width - element.size.width)/2;
-            element.render(g2d, x + offsetX , y, element.size.width, element.size.height);
-            
-            y+= element.size.height;
+        for (MathElement element : roots) {
+            int offsetX = 2; // (width - element.size.width) / 2;
+            element.render(g2d, x + offsetX, y, element.size.width, element.size.height);
+
+            y += element.size.height;
         }
     }
 
-    public void paint(Graphics2D g2d){
+    public void paint(Graphics2D g2d) {
         Dimension size = calculateSize(g2d);
-        render(g2d,0,0,size.width,size.height);
+        render(g2d, 0, 0, size.width, size.height);
     }
 
     @Override
@@ -111,20 +114,4 @@ public class MathFormula extends MathElement implements TypedNode {
     public String getType() {
         return "math.formula";
     }
-
-    /**
-     * Generates the code for the math formula, using a specific MathFormulaCodeGenerator object.
-     * @param result
-     * @param aThis
-     * @return 
-     */
-    /*
-    public void build(StringBuilder result, MathFormulaCodeGenerator codeGenerator) {
-        for(MathElement element: this.roots)
-        {
-            element.build(result,codeGenerator);
-            result.append('\n');
-        }
-    }
-    */
 }
