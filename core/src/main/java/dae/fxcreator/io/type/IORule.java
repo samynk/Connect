e0517b040471e205fca1a2655db480146fd4d792
Koen Samyn <samyn.koen@gmail.com>
2018-02-12 11:33:43 +0100

    removed references to shaders.

--- core/src/main/java/dae/fxcreator/io/type/IORule.java
+++ core/src/main/java/dae/fxcreator/io/type/IORule.java
@@ -1,50 +1,49 @@
 package dae.fxcreator.io.type;
 
 import dae.fxcreator.node.IONode;
-import dae.fxcreator.node.ShaderInput;
-import dae.fxcreator.node.ShaderType;
+import dae.fxcreator.node.NodeInput;
+import dae.fxcreator.node.IOType;
 
 /**
- * This class calculates the type of the output(s) given the type
- * of the inputs.
+ * This class calculates the type of the output(s) given the type of the inputs.
  * A rule command consists of a command and the inputs to apply the rule to.
+ *
  * @author Koen
  */
 public class IORule {
+
     /**
      * The rule command for this IORule.
      */
-    private String rule;
+    private final String rule;
 
-    public IORule(String rule)
-    {
+    public IORule(String rule) {
         this.rule = rule;
     }
 
     /**
      * Adapt the output types to the input types.
+     *
      * @param node the node to calculate the outputs for.
      */
-    public void calculateOutputTypes(IONode node){
+    public void calculateOutputTypes(IONode node) {
 
     }
 
     /**
      *
      */
-    private ShaderType max(ShaderInput ... inputs)
-    {
-        if ( inputs.length == 0)
+    private IOType max(NodeInput... inputs) {
+        if (inputs.length == 0) {
             return null;
-        else if ( inputs.length == 1)
+        } else if (inputs.length == 1) {
             return inputs[0].getIOType();
-        else{
+        } else {
             int order = -1;
-            ShaderType type = null;
-            for (int i = 0; i< inputs.length ; ++i)
-            {
-                ShaderType st = inputs[i].getIOType();
-                if (st.getOrder() > order){
+            IOType type = null;
+            for (int i = 0; i < inputs.length; ++i) {
+                IOType st = inputs[i].getIOType();
+                if (st.getOrder() > order) {
                     type = st;
                 }
             }
